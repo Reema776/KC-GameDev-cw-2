8b3724c7a2acf73417e7423535c91ecf814a1eaf
reema776 <reemaalmusaeileet@gmail.com>
2022-07-31 22:06:17 +0300

    reemaCW2

--- cw 2/Assets/cw2.cs
+++ cw 2/Assets/cw2.cs
@@ -10,20 +10,20 @@ public class cw2 : MonoBehaviour
     string heroSuperPower = "powered Armor";
 
         string villainName = "Thanos";
-        float vililanHeight = 2.52f;
+        float villainHeight = 2.52f;
         int villainAge = 1000;
     string villainSuperPower = "Omnipotent/infinity stones";
 
     void Start()
     {
         print("the super hero's name is " + heroName + " his hight is" + heroHeight + ", his age is" + heroAge + " years old and his superpower is " + heroSuperPower);
-        print("the vill hero's name is " + villainName + " his hight is" + vililanHeight + ", his age is around " + villainAge + " years old and his superpower is " + villainSuperPower);
+        print("the villain's name is " + villainName + " his hight is" + villainHeight + ", his age is around " + villainAge + " years old and his superpower is " + villainSuperPower);
         float ageDifference = villainAge - heroAge;
         print("age Difference is " + ageDifference);
         float heroHeight1 = 6.74f;
         string villainSuperPower1 = "None";
         print("the super hero's name is " + heroName + " his hight is" + heroHeight1 + ", his age is" + heroAge + " years old and his superpower is " + heroSuperPower);
-        print("the vill hero's name is " + villainName + " his hight is" + vililanHeight + ", his age is around " + villainAge + " years old and his superpower is " + villainSuperPower1 );
+        print("the villain's name is " + villainName + " his hight is" + villainHeight + ", his age is around " + villainAge + " years old and his superpower is " + villainSuperPower1 );
 
     }
 
