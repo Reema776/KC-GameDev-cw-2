947f472b8e7ce4df4d6bfb6dc3aa05b113fc31f8
reema776 <reemaalmusaeileet@gmail.com>
2022-07-31 21:53:11 +0300

    ReemaCW2

---
+++ cw 2/Assets/cw2.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cw2 : MonoBehaviour
+{
+        string heroName = "Iron man";
+        float heroHeight = 1.74f;
+        int heroAge = 40;
+    string heroSuperPower = "powered Armor";
+
+        string villainName = "Thanos";
+        float vililanHeight = 2.52f;
+        int villainAge = 1000;
+    string villainSuperPower = "Omnipotent/infinity stones";
+
+    void Start()
+    {
+        print("the super hero's name is " + heroName + " his hight is" + heroHeight + ", his age is" + heroAge + " years old and his superpower is " + heroSuperPower);
+        print("the vill hero's name is " + villainName + " his hight is" + vililanHeight + ", his age is around " + villainAge + " years old and his superpower is " + villainSuperPower);
+        float ageDifference = villainAge - heroAge;
+        print("age Difference is " + ageDifference);
+        float heroHeight1 = 6.74f;
+        string villainSuperPower1 = "None";
+        print("the super hero's name is " + heroName + " his hight is" + heroHeight1 + ", his age is" + heroAge + " years old and his superpower is " + heroSuperPower);
+        print("the vill hero's name is " + villainName + " his hight is" + vililanHeight + ", his age is around " + villainAge + " years old and his superpower is " + villainSuperPower1 );
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
